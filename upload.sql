f535171ed8d70b095234f6e646672257dc6dafb5
Eri Schon <erischon@gmail.com>
2021-02-10 15:24:28 +0100

    Update upload.sql

--- upload.sql
+++ upload.sql
@@ -7,3 +7,13 @@ COPY customer(id_user, first_name, last_name, phone_number, email, customer_type
 FROM 'C:\Users\Public\p6-data\customer.csv'
 DELIMITER ','
 CSV HEADER;
+
+COPY customer_address(address_1, address_2, zip_code, city)
+FROM 'C:\Users\Public\p6-data\customer_address.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY customeraddress(id_address, id_customer)
+FROM 'C:\Users\Public\p6-data\customeraddress.csv'
+DELIMITER ','
+CSV HEADER;
