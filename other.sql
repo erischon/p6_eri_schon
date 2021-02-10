06e3a1878ab61d833d50f8c0b7f572e699fdb8ed
Eri Schon <erischon@gmail.com>
2021-02-10 15:24:26 +0100

    Update other.sql

--- other.sql
+++ other.sql
@@ -1,2 +1,8 @@
 -- modification du length pour phone_number
 alter table customer alter column phone_number TYPE varchar(20);
+alter table shop alter column phone_number TYPE varchar(20);
+
+-- j'efface la table customer pour y remettre le .csv
+truncate customer cascade;
+
+alter table customer alter column first_name SET NOT NULL;
