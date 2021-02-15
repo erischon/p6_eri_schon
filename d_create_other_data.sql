8da0fe11f5369f2213e193e20e9e7687f17f399f
Eri Schon <erischon@gmail.com>
2021-02-15 09:40:24 +0100

    Create d_create_other_data.sql

---
+++ d_create_other_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO the_order (order_number, id_customer, id_shop, id_type, order_date, total_price)
+    VALUES ('150221PIZ015', '4', '1', '2', '2021-02-15 1:10:09', '19.94');
+
+COPY order_status(id_order, id_status, status_time, id_staff)
+FROM 'C:\Users\Public\p6-data\b_order_status.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY order_detail(id_order, id_product, quantity)
+FROM 'C:\Users\Public\p6-data\b_order_detail.csv'
+DELIMITER ','
+CSV HEADER;
